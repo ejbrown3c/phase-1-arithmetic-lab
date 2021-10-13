8cf27c4b3fe599483b2035535617a6904dce9a52
EJ Brown <ejbrown3c@gmail.com>
2021-10-13 12:52:38 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+
+var multiply = (num1 * num2 );
+
+var random = Math.floor(Math.random() > 0);
+
+
+let num3 = 12
+let num4 = 3
+
+var mod = (num3 / num4 );
+
+var max = Math.max (5, 10, 15, 20);
